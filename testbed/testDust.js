7a1da4a00405856fd1299f770f1ac043ac374aad
Olivia Chang <hello@oliviachang.me>
2018-11-12 19:20:30 -0800

    remove joint, everything is fixed to ground instead of body

--- testbed/testDust.js
+++ testbed/testDust.js
@@ -1,4 +1,6 @@
 function TestDust() {
+  this.time = 0;
+
   camera.position.y = 1;
   camera.position.z = 3;
 
@@ -12,16 +14,18 @@ function TestDust() {
   groundBox.vertices[3] = new b2Vec2(-4, -1);
   ground.CreateFixtureFromShape(groundBox, 0.0);
 
-  bd.type = b2_dynamicBody;
-  bd.allowSleep = false;
-  bd.position.Set(0, 1);
-  var body = world.CreateBody(bd);
-
+  // it's not really a ball, i can't figure out how to make it circular :(
   var ball = new b2CircleShape;
-  ball.position.Set(-2, 0);
+  // ball.position.Set(-10, 10);
   ball.radius = .7;
   this.ball = ball;
-  body.CreateFixtureFromShape(ball, 1.0);
+  ground.CreateFixtureFromShape(ball, 1.0);
+
+  // var ballDef = new b2BodyDef;
+  // ballDef.type = b2_dynamicBody;
+  // ballDef.position.Set(0, 1)
+  // ballDef.bullet = true;
+  // var ball = world.CreateBody(ballDef)
 
   // setup particles
   var psd = new b2ParticleSystemDef();
@@ -38,18 +42,16 @@ function TestDust() {
   
   var pgd = new b2ParticleGroupDef();
   pgd.shape = box;
-  pgd.groupFlags = b2_rigidParticleGroup;
+  pgd.flags = b2_wallParticle;
 
   var pg = particleSystem.CreateParticleGroup(pgd);
-
-  this.time = 0;
 }
 
 TestDust.prototype.Step = function() {
   world.Step(timeStep, velocityIterations, positionIterations);
   this.time += 1 / 60;
 
-  this.ball.position.Set(-2 + this.time*12, 0);
+  this.ball.position.Set(-4 + this.time*2, 1.25);
 
-  console.log(this.particleSystem.GetPositionBuffer())
+  // console.log(this.particleSystem.GetPositionBuffer())
 }
