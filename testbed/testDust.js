5bdd6a2abd01aac1d64922ba186dd0eb479c57eb
Olivia Chang <hello@oliviachang.me>
2018-12-02 21:37:53 -0800

    ball uses rigid particles

--- testbed/testDust.js
+++ testbed/testDust.js
@@ -1,66 +1,98 @@
 function TestDust() {
-  // set time to 0
-  // not entirely sure why it's necessary because it doesn't appear in any of the other simulations,
-  // but everything breaks when i remove it
-  this.time = 0;
+  /* notes
+  - only one particle system def
+  - only one particle system
+  - create a new particle group def for each particle group
+  */
 
   // set camera position
-  camera.position.y = 1;
+  camera.position.y = 0;
   camera.position.z = 3;
 
   // bd is a bodydef, not a real body, which all other bodies can be made from
   var bd = new b2BodyDef();
-  // create ground from body
   var ground = world.CreateBody(bd);
 
-  // set up groundbox, which is a shape
+  // move this to a function later
+  // ground
   var groundBox = new b2PolygonShape();
-  groundBox.vertices[0] = new b2Vec2(-4, -1);
-  groundBox.vertices[1] = new b2Vec2(4, -1);
-  groundBox.vertices[2] = new b2Vec2(4, -1);
-  groundBox.vertices[3] = new b2Vec2(-4, -1);
-  // attach groundBox shape to ground using fixture
-  ground.CreateFixtureFromShape(groundBox, 0.0);
+  var vertices = groundBox.vertices;
+  vertices.push(new b2Vec2(-4, -3));
+  vertices.push(new b2Vec2(4, -3));
+  vertices.push(new b2Vec2(4, -2));
+  vertices.push(new b2Vec2(-4, -2));
+  ground.CreateFixtureFromShape(groundBox, 0);
 
-  // it's not really a ball, i can't figure out how to make it circular :(
-  var ball = new b2CircleShape;
-  ball.position.Set(-3, 1.5);
-  ball.radius = .7;
-  this.ball = ball; // this.ball is used in the Step function
-  // attach ball shape to ground using fixture
-  // otherwise it'll just drop straight down
-  ground.CreateFixtureFromShape(ball, 1.0);
+  // set some walls so it doesn't slide off
+  var leftSideBox = new b2PolygonShape();
+  var vertices = leftSideBox.vertices;
+  vertices.push(new b2Vec2(-4, -2));
+  vertices.push(new b2Vec2(-3, -2));
+  vertices.push(new b2Vec2(-3, 3));
+  vertices.push(new b2Vec2(-4, 3));
+  ground.CreateFixtureFromShape(leftSideBox, 0);
+
+  var rightSideBox = new b2PolygonShape();
+  var vertices = rightSideBox.vertices;
+  vertices.push(new b2Vec2(4, -2));
+  vertices.push(new b2Vec2(3, -2));
+  vertices.push(new b2Vec2(3, 3));
+  vertices.push(new b2Vec2(4, 3));
+  ground.CreateFixtureFromShape(rightSideBox, 0);
 
   // setup particles
   var psd = new b2ParticleSystemDef();
   // size of individual particles
   psd.radius = 0.025; 
-  psd.dampingStrength = 0.2;
-
+  // psd.dampingStrength = 0.2;
   var particleSystem = world.CreateParticleSystem(psd);
-  this.particleSystem = particleSystem;
 
-  // make a shape which will be the shape of the particles
-  var box = new b2PolygonShape();
-  box.SetAsBoxXYCenterAngle(0.5, 2, new b2Vec2(0, 1.0), 0);
-  
+  // metal ball
+  var circle = new b2CircleShape();
+  circle.position.Set(-2, 1);
+  circle.radius = 0.5;
   var pgd = new b2ParticleGroupDef();
-  // use the box we made just now as the shape for the particles
-  pgd.shape = box;
-  // can set the group's entire flags as either solid or rigid
-  // https://google.github.io/liquidfun/Programmers-Guide/html/md__chapter11__particles.html#pb
-  pgd.groupFlags = b2_rigidParticleGroup;
-  // can also set the particles as elastic, powder, spring, etc
-  // pgd.flags = b2_elasticParticle
+  pgd.groupFlags = b2_rigidParticleGroup | b2_solidParticleGroup;
+  pgd.flags = b2_particleContactListenerParticle;
+  pgd.shape = circle;
+  pgd.color.Set(255, 0, 0, 255);
+  // make it move
+  pgd.linearVelocity = (new b2Vec2(180, -100)); // degrees, speed+direction
+  var ball = particleSystem.CreateParticleGroup(pgd);
 
-  var pg = particleSystem.CreateParticleGroup(pgd);
+  // var f = ground.GetWorldVector(new b2Vec2(180, -100));
+  // var p = ground.GetWorldPoint(new b2Vec2(0, 10));
+  // ball.ApplyForce(f, p, true);
+
+  // wall
+  var box = new b2PolygonShape();
+  pgd = new b2ParticleGroupDef;
+  box.SetAsBoxXY(0.5, 2);
+  pgd.groupFlags = b2_rigidParticleGroup;
+  pgd.flags = b2_particleContactListenerParticle;
+  pgd.position.Set(1,0);
+  pgd.shape = box;
+  pgd.color.Set(0,0,255,255);
+  particleSystem.CreateParticleGroup(pgd);
+
+  world.SetContactListener(this);
 }
+
+TestDust.prototype.BeginContactBody = function(contact) {
+  // if (contact.GetFixtureA().GetUserData() !== null ||
+  //   contact.GetFixtureB().GetUserData() !== null) {
+  //   var worldManifold = contact.GetWorldManifold();
+  //   this.contactPoint = worldManifold.GetPoint(0);
+  //   this.contact = true;
+  // }
+  console.log('contact')
+};
 
 TestDust.prototype.Step = function() {
   world.Step(timeStep, velocityIterations, positionIterations);
-  // increment time, which is weird
-  this.time += 1 / 60;
 
-  // move the ball 2 units per second
-  this.ball.position.Set(-3 + this.time*2, 1.5);
+  // if (this.contact) {
+  //   console.log('this.contact')
+  // }
+
 }
